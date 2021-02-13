e3e6232b0a538437083e48a13c97cf78abe1eaaa
Madoane45 <Madoane45@gmail.com>
2021-02-13 16:11:01 -0500

    commit tree 3ee4b175dc5647bcc22d0860db1d8e9b47ee3449

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+require "pry"
+class School
+attr_accessor :school, :roster, :student
+ def initialize(school)
+    @school = school
+    @roster = {}
+ end
+ def add_student(name,grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+ end
+ def grade(grade)
+    @roster[grade]
+end
+def sort
+    hash = {}
+    @roster.each do |grade, students|
+        # binding.pry
+        hash[grade] = students.sort
+    end
+    hash
+    end
+
+
+end
